7436f0cf7ea52562912c4b8edf716558f9ad43d5
jameswood <james_s_wood@outlook.com>
2021-03-15 13:11:09 -0400

    docs: removed comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
-/*!
-    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
+
+(function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
